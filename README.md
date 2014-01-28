12f2306ad68d5ff0a07f963693400045c7dcbdc4
Benoit Asselin <contact@ab-d.fr>
2014-01-28 22:41:23 +0100

    no message

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-jquery-background-stretch-2x
-============================
+$.backgroundStretch2x()
+=======================
+This jQuery plugin can resize images according the page size or its container.
+
+The plugin works in 4 steps :
+* It loads the image in low resolution *( image-1.jpg )*,
+* Then, this same image in high resolution *( image-1-2x.jpg )*,
+* And, the other images are loaded in high resolution,
+* Finally, the fade starts...
+
+Installation
+------------
+
+The image filname be named like this *(the resolution is free, but must be double for the file "-2x.ext")*.
+**image-1.jpg** (800x600) et **image-1-2x.jpg** (1600*1200)
+
+```html
+<script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+<script src="jquery.backgroundStretch2x.min.js"></script>
+<script>
+$(function(){
+	// Use a page BODY
+	$.backgroundStretch2x( ['images/image-1-2x.jpg', 'images/image-2-2x.jpg', 'images/image-3-2x.jpg'] );
+	$('body').backgroundStretch2x( ['images/image-1-2x.jpg', 'images/image-2-2x.jpg', 'images/image-3-2x.jpg'] );
+	
+	// Use a DIV
+	$('#my-div').backgroundStretch2x( ['images/image-1-2x.jpg', 'images/image-2-2x.jpg', 'images/image-3-2x.jpg'], {
+		wait: 6000,
+		fade: 1000,
+		suffle: true
+	});
+});
+</script>
+```
+
+Options
+-------
+
+| Name | Description | Type | Default |
+|------|-------------|------|---------|
+| `wait` | Waiting time between 2 images. | Integer | 5000 |
+| `fade` | Time of fade. | Integer | 2000 |
+| `shuffle` | Shuffle images. | Boolean | false |
+| `centerX` | Percentage of centering image in X *(de 0 à 1)* | Float | 0.5 |
+| `centerY` | Percentage of centering image in Y. *(de 0 à 1)* | Float | 0.5 |
+| `proportional` | Resize images proportionally. | Boolean | true |
+| `onLoad` | All images are loaded. | Function | null |
+| `onChange` | The fade will start. | Function | null |
+| `onComplete` | The fade is completed. | Function | null |
+| `styles` | DIV style `{ left:0, top:0, overflow:'hidden', zIndex:-32000 }`. | Object | { } |
+| `template` | HTML `<div class="background-stretch-2x"></div>`. | String | `<div />` |
+
---
+++ jquery.backgroundStretch2x.js
@@ -0,0 +1,179 @@
+/*!
+ * jquery.backgroundStrect2x.js v1.0.0
+ * (c) 2014, Benoit Asselin contact(at)ab-d.fr
+ * MIT Licence
+ */
+
+
+
+;(function($, window, undefined) {
+	'use strict';
+	
+	
+	
+	/**
+	 * Background stretch 2x
+	 * @param {Array} images2x
+	 * @param {Object} options
+	 */
+	$.fn.backgroundStretch2x = function(images2x, options) {
+		options = $.extend({
+			// defaults
+			wait: 5000, // ms
+			fade: 2000, // ms
+			shuffle: false, // Boolean
+			centerX: 0.5, // Number [0..1]
+			centerY: 0.5, // Number [0..1]
+			proportional: true, // Boolean
+			onLoad: null, // Function
+			onChange: null, // Function
+			onComplete: null, // Function
+			styles: { left:0, top:0, overflow:'hidden', zIndex:-32000 }, // Object
+			template: '<div class="background-stretch-2x"></div>' // html
+			}, options);
+		
+		if(!images2x || !images2x.length) { return this; }
+		var $win = $(window);
+		
+		return this.each(function() {
+			// Prepare
+			var isBody = ('body' === this.tagName.toLowerCase());
+			var $this = $(this);
+			var $background = $($.parseHTML(options.template))
+						.css(options.styles)
+						.css({position:(isBody ? 'fixed' : 'absolute')})
+						.appendTo($this);
+			if(options.shuffle) {
+				images2x.sort(function(){ return (Math.round(Math.random()) - 0.5); });
+			}
+			var images1x = [];
+			for(var i = 0; i < images2x.length; i++) {
+				images1x[i] = images2x[i].replace('-2x.','.');
+			}
+			
+			function windowResize() {
+				var $elt = (isBody ? $win : $this);
+				var eltWidth = $elt.outerWidth(),
+				    eltHeight = $elt.outerHeight(),
+				    eltRatio = eltWidth / eltHeight;
+				$background
+					.css({width:eltWidth, height:eltHeight})
+					.children().each(function() {
+						var $i = $(this);
+						var newWidth = 0, newHeight = 0;
+						var newLeft = 0, newTop = 0;
+						var newRatio = 0;
+						if(options.proportional) {
+							var imageWidth = $i.data('initWidth'),
+							    imageHeight = $i.data('initHeight'),
+							    imageRatio = $i.data('initRatio');
+							if(eltRatio < imageRatio) {
+								newRatio = eltHeight / imageHeight;
+								newHeight = imageHeight * newRatio;
+								newWidth = newHeight * imageRatio;
+								newLeft = - Math.floor((newWidth - eltWidth) * options.centerX);
+							} else {
+								newRatio = eltWidth / imageWidth;
+								newWidth = imageWidth * newRatio;
+								newHeight = newWidth / imageRatio;
+								newTop = - Math.floor((newHeight - eltHeight) * options.centerY);
+							}
+						} else {
+							newWidth = eltWidth;
+							newHeight = eltHeight;
+						}
+						$i.css({
+							left:newLeft, top:newTop,
+							width:Math.round(newWidth), height:Math.round(newHeight)
+						});
+					});
+			}
+			
+			function buildImage(src, onComplete) {
+				if(!src) { return; }
+				var is2x = (-1 !== src.indexOf('-2x.'));
+				var $image = $('<img class="bs-img-' + (!is2x ? '1x' : '2x') + '"/>');
+				$image
+					.data('loaded', false)
+					.load(function() {
+						var $i = $(this);
+						$i	.css({position:'absolute'})
+							.data('loaded', true)
+							.data('initWidth', $i.width())
+							.data('initHeight', $i.height())
+							.data('initRatio', $i.width() / $i.height());
+						if(isNaN($i.data('initRatio'))) { // fix.MsIE 7-
+							setTimeout(function() {
+								$i.trigger('load');
+							}, 50);
+							return;
+						}
+						windowResize();
+						if('function' === typeof onComplete) {
+							onComplete();
+						}
+					})
+					.error(function() {
+						if(window.console) {
+							console.log('Error: ' + src);
+						}
+					})
+					.attr('src', src);
+				$background.prepend($image);
+			}
+			
+			function prepareImages() {
+				$background.children('.bs-img-1x').remove();
+				if(images2x.length > 1) {
+					$.each(images2x, function(idx, src) {
+						if(!idx) { return; }
+						buildImage(src, imagesReady);
+					});
+				} else {
+					if('function' === typeof options.onLoad) { options.onLoad(); }
+				}
+			}
+			
+			function imagesReady() {
+				var loadedCount = 0;
+				$background.children().each(function() {
+					if($(this).data('loaded')) {
+						loadedCount++;
+					}
+				});
+				if(loadedCount >= images2x.length) {
+					// Fade
+					if('function' === typeof options.onLoad) { options.onLoad(); }
+					setInterval(imagesTransition, (options.wait + options.fade));
+				}
+			}
+			
+			function imagesTransition() {
+				if('function' === typeof options.onChange) { options.onChange(); }
+				$background.children().last()
+					.animate({opacity:0}, options.fade, function() {
+						$(this)
+							.prependTo($background)
+							.css({opacity:''});
+						if('function' === typeof options.onComplete) { options.onComplete(); }
+					});
+			}
+			
+			// Start
+			$win.on('resize orientationchange', windowResize);
+			buildImage(images1x[0], function() {
+				buildImage(images2x[0], prepareImages);
+			});
+			return this;
+		});
+	};
+	
+	$.backgroundStretch2x = function(images2x, options) {
+		$('body').backgroundStretch2x(images2x, options);
+	};
+	
+	
+	
+})(jQuery, window);
+
+
